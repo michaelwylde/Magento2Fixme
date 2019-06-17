3fe94791e158e9657120736d18de446ac68375f2
Michael Wylde <michael.wylde@aligent.com.au>
2019-06-18 08:08:46 +0930

    Added bare module that causes database tables to not be created in empty database.

---
+++ Console/Command/Fixme.php
@@ -0,0 +1,17 @@
+<?php
+namespace Swimwear\Fixme\Console\Command;
+
+class Fixme extends \Symfony\Component\Console\Command\Command
+{
+    public function __construct(
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('swimwear:fixme')
+            ->setDescription('Please fixme');
+    }
+}
